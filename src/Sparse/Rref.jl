813cb513fa32218f1daf6c256bcb1e6562877e19
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-09-06 13:34:18 +0200

    First kinda working implementation of Markowitz pivoting

--- src/Sparse/Rref.jl
+++ src/Sparse/Rref.jl
@@ -125,6 +125,204 @@ function _add_row_to_rref!(M::SMat{T}, v::SRow{T}) where { T <: FieldElem }
   return true
 end
 
+# Adds t*A.rows[l] to A.rows[k] (the ordering is different than in add_scaled_row!!!!)
+function _add_scaled_row_with_transpose!(A::SMat{T}, k::Int, l::Int, t::T, AT::Vector{Vector{Int}}, t1::T = base_ring(A)()) where {T <: FieldElement}
+  a = A.rows[l]
+  b = A.rows[k]
+
+  i = 1
+  j = 1
+  while i <= length(a) && j <= length(b)
+    if a.pos[i] < b.pos[j]
+      t1 = mul!(t1, t, a.values[i])
+      if !is_zero(t1)
+        insert!(b.pos, j, a.pos[i])
+        insert!(b.values, j, deepcopy(t1))
+        j += 1
+        A.nnz += 1
+        jj = searchsortedfirst(AT[a.pos[i]], k)
+        @assert jj > length(AT[a.pos[i]]) || AT[a.pos[i]][jj] > k
+        insert!(AT[a.pos[i]], jj, k)
+      end
+      i += 1
+    elseif a.pos[i] > b.pos[j]
+      j += 1
+    else
+      t1 = mul!(t1, t, a.values[i])
+      b.values[j] = addeq!(b.values[j], t1)
+
+      if is_zero(b.values[j])
+        deleteat!(b.values, j)
+        deleteat!(b.pos, j)
+        A.nnz -= 1
+        jj = searchsortedfirst(AT[a.pos[i]], k)
+        @assert AT[a.pos[i]][jj] == k
+        deleteat!(AT[a.pos[i]], jj)
+      else
+        j += 1
+      end
+      i += 1
+    end
+  end
+  while i <= length(a)
+    t1 = mul!(t1, t, a.values[i])
+    if !is_zero(t1)
+      push!(b.pos, a.pos[i])
+      push!(b.values, deepcopy(t1))
+      A.nnz += 1
+      jj = searchsortedfirst(AT[a.pos[i]], k)
+      @assert jj > length(AT[a.pos[i]]) || AT[a.pos[i]][jj] > k
+      insert!(AT[a.pos[i]], jj, k)
+    end
+    i += 1
+  end
+  return nothing
+end
+
+function rref_markowitz!(A::SMat{T}) where {T <: FieldElement}
+  # "Pseudo" transpose of A: AT[c] is the list indices r such that A[r, c] is
+  # non-zero
+  AT = Vector{Vector{Int}}()
+  for i in 1:ncols(A)
+    push!(AT, Vector{Int}())
+  end
+  for i in 1:nrows(A)
+    for j in A.rows[i].pos
+      push!(AT[j], i)
+    end
+  end
+
+  # For a column c, weights[c] is the list of pairs (r, w) where w gives the
+  # weight of the entry A[r, c].
+  # The weight of A[r, c] is defined as (R - 1)*(C - 1), where R is the number
+  # of non-zero entries in row r and C the number of non-zero entries in column
+  # C.
+  # The Vector weights[c] is sorted by increasing weight.
+  # TODO: Possibly, replace this with a fancier data structure.
+  weights = Vector{Vector{Tuple{Int, Int}}}()
+  for c in 1:ncols(A)
+    push!(weights, Vector{Tuple{Int, Int}}())
+    for r in AT[c]
+      lr = length(A.rows[r])
+      lc = length(AT[c])
+      push!(weights[c], (r, (lr - 1)*(lc - 1)))
+    end
+    sort!(weights[c], lt = (x, y) -> x[2] < y[2])
+  end
+
+  # If pivot_cols[c] == r with r != 0, then the pivot of column c is in row r
+  # If pivot_rows[r] == c with c != 0, then the pivot of row r is in column c
+  pivot_cols = zeros(Int, ncols(A))
+  pivot_rows = zeros(Int, nrows(A))
+
+  t = base_ring(A)()
+  t1 = base_ring(A)()
+  t2 = base_ring(A)()
+  while true
+    c_min = 0
+    min_weight = (nrows(A) - 1)*(ncols(A) - 1)
+    for c in 1:ncols(A)
+      !is_zero(pivot_cols[c]) && continue
+      is_empty(weights[c]) && continue
+      if min_weight >= weights[c][1][2]
+        c_min = c
+        min_weight = weights[c][1][2]
+      end
+    end
+    c_min == 0 && break
+    r_min = weights[c_min][1][1]
+    @assert pivot_cols[c_min] == 0
+    pivot_cols[c_min] = r_min
+    @assert is_zero(pivot_rows[r_min])
+    pivot_rows[r_min] = c_min
+    weights[c_min] = Vector{Tuple{Int, Int}}()
+    a = A.rows[r_min]
+    for c in a.pos
+      c == c_min && continue
+      j = findfirst(x -> x[1] == r_min, weights[c])
+      @assert !isnothing(j)
+      deleteat!(weights[c], j)
+    end
+
+    p = searchsortedfirst(a.pos, c_min)
+    if !is_one(a.values[p])
+      t = inv(a.values[p])
+      for j = 1:length(a)
+        j == p && continue
+        a.values[j] = mul!(a.values[j], a.values[j], t)
+      end
+      a.values[p] = one(base_ring(A))
+    end
+    for r in copy(AT[c_min])
+      r == r_min && continue
+      b = A.rows[r]
+      pb = searchsortedfirst(b.pos, c_min)
+      @assert pb <= length(b) && b.pos[pb] == c_min
+
+      for c in b.pos
+        j = findfirst(x -> x[1] == r, weights[c])
+        if !isnothing(j)
+          deleteat!(weights[c], j)
+        end
+      end
+
+      t = -b.values[pb]
+      _add_scaled_row_with_transpose!(A, r, r_min, t, AT, t1)
+      !is_zero(pivot_rows[r]) && continue
+
+      for c in b.pos
+        w = (length(b) - 1)*(length(AT[c]) - 1)
+        rw = (r, w)
+        j = searchsortedfirst(weights[c], rw, lt = (x, y) -> x[2] < y[2])
+        insert!(weights[c], j, rw)
+      end
+    end
+  end
+
+  # At this point, we found all the pivots there
+  # Now go over the entries of A and make sure that every entry is to the right
+  # of its pivot (this is only relevant if A does not have full rank)
+
+  for c in 1:ncols(A)
+    for r in copy(AT[c])
+      pivot_rows[r] <= c && continue
+
+      a = A.rows[r]
+      c_new = a.pos[1]
+      pivot_cols[c_new] = r
+      pivot_cols[pivot_rows[r]] = 0
+      pivot_rows[r] = c_new
+      # We messed up: the pivot in row r has to be in column c_new
+
+      if !is_one(a.values[1])
+        t = inv(a.values[1])
+        for j = 2:length(a)
+          a.values[j] = mul!(a.values[j], a.values[j], t)
+        end
+        a.values[1] = one(base_ring(A))
+      end
+
+      for i in copy(AT[c_new])
+        i == r && continue
+        j = searchsortedfirst(A.rows[i].pos, c_new)
+        @assert A.rows[i].pos[j] == c_new
+
+        t = -A.rows[i].values[j]
+        _add_scaled_row_with_transpose!(A, i, r, t, AT, t1)
+      end
+    end
+  end
+
+  # Final step: sort the rows
+  A.rows = A.rows[sortperm(pivot_rows)]
+  while isempty(A.rows[1])
+    deleteat!(A.rows, 1)
+    A.r -= 1
+  end
+
+  return A
+end
+
 ###############################################################################
 #
 #   Kernel
